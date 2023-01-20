a8d690810da293fbad0303ce5f72689b78015e3f
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-01-20 13:52:52 +0200

    config registion with backend

--- client/src/validate/ValidateForm.js
+++ client/src/validate/ValidateForm.js
@@ -3,23 +3,28 @@ export const validationForm = (type, data) => {
   var flagError = true;
 
   if (type == "register") {
+    if (data.confirmPassword != data.password) {
+      errors.push("confirm password not equal password");
+      flagError = false;
+    }
     if (
       data.confirmPassword.trim() == "" ||
-      data.Password.trim() == "" ||
-      data.FirstName.trim() == "" ||
-      data.LastName.trim() == ""
+      data.password.trim() == "" ||
+      data.firstName.trim() == "" ||
+      data.lastName.trim() == ""
     ) {
       errors.push("please enter filed");
       flagError = false;
       return errors;
     }
   }
-  if (data.confirmPassword != data.Password) {
-    errors.push("confirm password not equal password");
+
+  if (data.email.trim() == "") {
+    errors.push("email not valid");
     flagError = false;
   }
-  if (data.Email.trim() == "") {
-    errors.push("email not valid");
+  if (data.password == "") {
+    errors.push("please enter password");
     flagError = false;
   }
   return flagError ? true : errors;
