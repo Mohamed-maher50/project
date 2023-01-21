b16018638f58fa58a44cdbf4ed51cadac9ca7a5f
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-01-21 15:00:57 +0200

    add notifation component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Login from "./components/Login/Login";
-
+import NotifcationNav from "./components/NotifcationNav/NotifcationNav";
 import Register from "./components/register/Register";
 import Avatar from "./pages/Avatar";
 import Home from "./pages/Home";
 function App() {
+  const [notifcationStatus, setNotifcationStatus] = useState(true);
+  const notifcationStatusHandler = () => {
+    setNotifcationStatus((prev) => !prev);
+  };
   return (
-    <div className="App h-screen">
+    <div className="App h-screen min-h-screen   overflow-auto">
+      <NotifcationNav
+        notifcationStatusHandler={notifcationStatusHandler}
+        notifcationStatus={notifcationStatus}
+      />
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={<Home notifcationStatusHandler={notifcationStatusHandler} />}
+        />
         <Route path="/avatar" element={<Avatar />} />
       </Routes>
     </div>
