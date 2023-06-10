775745abcae2afe5a77b2cf6e4cc1642feec4720
‪mohamed maher‬‏ <78468030+Mohamed-maher50@users.noreply.github.com>
2023-06-10 10:33:54 +0300

    Update v_user.js

--- server/validation/v_user.js
+++ server/validation/v_user.js
@@ -15,7 +15,7 @@ exports. v_register = [
   check("NationalID").trim().not().isEmpty().withMessage("NationalID is required"),
   check("email").trim().normalizeEmail().not().custom(checkEmailExist).not().withMessage("this account already exist"),
   check("fullName").trim().not().isEmpty().withMessage("fullName is required"),
-  check("birthDay").trim().not().isEmpty().isDate().withMessage("date is required"),
+  check("birthDay").trim().not().isEmpty().withMessage("date is required"),
   check("city").trim().not().isEmpty(),
   check("password").isLength({ min: 7 }).withMessage(" password should be at least 7 characters. ").not().isEmpty().trim().custom((password, { req }) => {  if (password != req.body.confirmPassword)    return Promise.reject("password not equal confirm password");  return true;})
   ,v_layer
